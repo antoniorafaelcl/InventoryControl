d1c09e23f321f5cfebf3bd0f1a4418557c30d1a1
Antonio Rafael <antoniorafaelcl@gmail.com>
2018-05-10 17:46:19 -0300

    Criação do Projeto

---
+++ src/main/java/com/antoniorafaelcl/InvetoryControl/InvetoryControlApplication.java
@@ -0,0 +1,12 @@
+package com.antoniorafaelcl.InvetoryControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvetoryControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvetoryControlApplication.class, args);
+	}
+}
